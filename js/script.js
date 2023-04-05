a58aa3c9ce8567d73a2d72d50c56cd794e916144
Eugenio Lorenzi <lorenzie@mariva.com.ar>
2023-04-05 15:35:17 -0300

    push pog

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var playerScore = 0;
+var computerScore = 0;
+var playButton = document.getElementById("play");
+var resetButton = document.getElementById("reset");
+var playerScoreDisplay = document.getElementById("playerScore");
+var computerScoreDisplay = document.getElementById("computerScore");
+
+function playPOG() {
+	// Lógica para lanzar los POGs
+}
+
+function updateScore(result) {
+	// Lógica para actualizar los puntajes
+}
+
+function resetGame() {
+	// Lógica para reiniciar el juego
+}
+
+playButton.addEventListener("click", playPOG);
+resetButton.addEventListener("click", resetGame);
